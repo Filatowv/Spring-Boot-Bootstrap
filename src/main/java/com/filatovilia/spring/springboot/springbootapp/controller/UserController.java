7132ebac8cdf28011955406831bc3bf2bf1bde55
Ilia Filatov <filatov_26@mail.ru>
2021-12-17 19:23:38 +0300

    refactoring 'Admin_info' file

--- src/main/java/com/filatovilia/spring/springboot/springbootapp/controller/UserController.java
+++ src/main/java/com/filatovilia/spring/springboot/springbootapp/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 
