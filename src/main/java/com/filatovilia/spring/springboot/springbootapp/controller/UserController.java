0fddf5402dd77840f7969445c141ff71d7307b0f
Ilia Filatov <filatov_26@mail.ru>
2021-12-17 13:23:02 +0300

    refactoring 'admin_info' file

--- src/main/java/com/filatovilia/spring/springboot/springbootapp/controller/UserController.java
+++ src/main/java/com/filatovilia/spring/springboot/springbootapp/controller/UserController.java
@@ -8,12 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.HashSet;
@@ -87,10 +87,11 @@ public class UserController {
 		return "redirect:/admin";
 	}
 
-	// удаление
+
 	@RequestMapping("/admin/delete/{id}")
-	public String deleteProduct(@PathVariable(name = "id") int id) {
+	public String deleteProduct(@PathVariable("id") long id) {
 		userService.deleteUser(id);
 		return "redirect:/admin";
 	}
+
 }
